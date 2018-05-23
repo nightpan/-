b7ce60d0f208f60573307f09978eb145390547bd
張筆翔 <zhangbixiang@zhangbixiangde-MacBook-Pro.local>
2018-05-23 14:51:08 +0800

    oiu

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/jjjj60110/pen/MGxzMR.
+
+ 
---
+++ js/index.js
@@ -0,0 +1,814 @@
+var data1 = [
+    {
+        ticker: "基隆市",
+        po:1,
+        revenuechange: 20,
+        mktcap: 3
+    },
+    {
+        ticker: "台北市",
+        po:2,
+        revenuechange: -13,
+        mktcap: -63
+    },
+    {
+        ticker: "新北市",
+        po:3,
+        revenuechange: -16,
+        mktcap: -44
+    },
+    {
+        ticker: "桃園市",
+        po:4,
+        revenuechange: -10,
+        mktcap: -16
+    },
+    {
+        ticker: "新竹市",
+        po:5,
+        revenuechange: -9,
+        mktcap: -5
+    },
+    {
+        ticker: "新竹縣",
+        po:6,
+        revenuechange: 26,
+        mktcap: 10
+    },
+    {
+        ticker: "宜蘭縣",
+        po:7,
+        revenuechange: 2,
+        mktcap: 1
+    },
+    {
+        ticker: "苗栗縣",
+        po:8,
+        revenuechange: -8,
+        mktcap: -4
+    },
+    {
+        ticker: "台中市",
+        po:9,
+        revenuechange: -5,
+        mktcap: -14
+    },
+    {
+        ticker: "彰化縣",
+        po:10,
+        revenuechange: -11,
+        mktcap: -9
+    },
+    {
+        ticker: "南投縣",
+        po:11,
+        revenuechange: -8,
+        mktcap: -3
+    },
+    {
+        ticker: "雲林縣",
+        po:12,
+        revenuechange: -17,
+        mktcap: -8
+    },
+    {
+        ticker: "嘉義市",
+        po:13,
+        revenuechange: -2,
+        mktcap: -1
+    },
+    {
+        ticker: "嘉義縣",
+        po:14,
+        revenuechange: -22,
+        mktcap: -7
+    },
+    {
+        ticker: "台南市",
+        po:15,
+        revenuechange: -7,
+        mktcap: -14
+    },
+    {
+        ticker: "高雄市",
+        po:16,
+        revenuechange: -6,
+        mktcap: -16
+    },
+    {
+        ticker: "屏東縣",
+        po:17,
+        revenuechange: 2,
+        mktcap: 1
+    },
+    {
+       ticker: "台東縣",
+        po:18,
+        revenuechange: -6,
+        mktcap: -1
+    },
+    {
+        ticker: "花蓮縣",
+        po:19,
+        revenuechange: -18,
+        mktcap: -9
+    },
+    {
+        ticker: "澎湖縣",
+        po:20,
+        revenuechange: -10,
+        mktcap: -1
+    },
+    {
+        ticker: "金門縣",
+        po:21,
+        revenuechange: -7,
+        mktcap: -1
+    },
+    {
+        ticker: "連江縣",
+        po:22,
+        revenuechange: 0,
+        mktcap: 0
+    }
+];
+var data2 = [ 
+    {
+        ticker: "連江縣",
+        po:22,
+        revenuechange: 0,
+        mktcap: 0
+    },
+    {
+        ticker: "金門縣",
+        po:21,
+        revenuechange: -7,
+        mktcap: -1
+    },
+    {
+        ticker: "澎湖縣",
+        po:20,
+        revenuechange: -10,
+        mktcap: -1
+    },
+    {
+        ticker: "花蓮縣",
+        po:19,
+        revenuechange: -18,
+        mktcap: -9
+    },    
+    {
+       ticker: "台東縣",
+        po:18,
+        revenuechange: -6,
+        mktcap: -1
+    },  
+    {
+        ticker: "屏東縣",
+        po:17,
+        revenuechange: 2,
+        mktcap: 1
+    },
+    {
+        ticker: "高雄市",
+        po:16,
+        revenuechange: -6,
+        mktcap: -16
+    },
+    {
+        ticker: "台南市",
+        po:15,
+        revenuechange: -7,
+        mktcap: -14
+    },
+    {
+        ticker: "嘉義縣",
+        po:14,
+        revenuechange: -22,
+        mktcap: -7
+    },
+    {
+        ticker: "嘉義市",
+        po:13,
+        revenuechange: -2,
+        mktcap: -1
+    },
+    {
+        ticker: "雲林縣",
+        po:12,
+        revenuechange: -17,
+        mktcap: -8
+    },
+    {
+        ticker: "南投縣",
+        po:11,
+        revenuechange: -8,
+        mktcap: -3
+    },
+    {
+        ticker: "彰化縣",
+        po:10,
+        revenuechange: -11,
+        mktcap: -9
+    },
+    {
+        ticker: "台中市",
+        po:9,
+        revenuechange: -5,
+        mktcap: -14
+    },
+    {
+        ticker: "苗栗縣",
+        po:8,
+        revenuechange: -8,
+        mktcap: -4
+    },
+    {
+        ticker: "宜蘭縣",
+        po:7,
+        revenuechange: 2,
+        mktcap: 1
+    },
+    {
+        ticker: "新竹縣",
+        po:6,
+        revenuechange: 26,
+        mktcap: 10
+    },
+    {
+        ticker: "新竹市",
+        po:5,
+        revenuechange: -9,
+        mktcap: -5
+    }, 
+    {
+        ticker: "桃園市",
+        po:4,
+        revenuechange: -10,
+        mktcap: -16
+    },
+    {
+        ticker: "新北市",
+        po:3,
+        revenuechange: -16,
+        mktcap: -44
+    },
+    {
+        ticker: "台北市",
+        po:2,
+        revenuechange: -13,
+        mktcap: -63
+    },
+    {
+        ticker: "基隆市",
+        po:1,
+        revenuechange: 20,
+        mktcap: 3
+    },
+];
+var data3 = [
+    {
+        ticker: "新竹縣",
+        po:6,
+        revenuechange: 26,
+        mktcap: 10
+    },
+    {
+        ticker: "基隆市",
+        po:1,
+        revenuechange: 20,
+        mktcap: 3
+    },
+    {
+        ticker: "宜蘭縣",
+        po:7,
+        revenuechange: 2,
+        mktcap: 1
+    },
+    {
+        ticker: "屏東縣",
+        po:17,
+        revenuechange: 2,
+        mktcap: 1
+    },
+     {
+        ticker: "連江縣",
+        po:22,
+        revenuechange: 0,
+        mktcap: 0
+    },
+    {
+        ticker: "嘉義市",
+        po:13,
+        revenuechange: -2,
+        mktcap: -1
+    },
+    {
+       ticker: "台東縣",
+        po:18,
+        revenuechange: -6,
+        mktcap: -1
+    },
+    {
+        ticker: "金門縣",
+        po:21,
+        revenuechange: -7,
+        mktcap: -1
+    },
+    {
+        ticker: "澎湖縣",
+        po:20,
+        revenuechange: -10,
+        mktcap: -1
+    },
+    {
+        ticker: "南投縣",
+        po:11,
+        revenuechange: -8,
+        mktcap: -3
+    },
+    {
+        ticker: "苗栗縣",
+        po:8,
+        revenuechange: -8,
+        mktcap: -4
+    },
+    {
+        ticker: "新竹市",
+        po:5,
+        revenuechange: -9,
+        mktcap: -5
+    },
+    {
+        ticker: "嘉義縣",
+        po:14,
+        revenuechange: -22,
+        mktcap: -7
+    },
+    {
+        ticker: "雲林縣",
+        po:12,
+        revenuechange: -17,
+        mktcap: -8
+    },
+    {
+        ticker: "彰化縣",
+        po:10,
+        revenuechange: -11,
+        mktcap: -9
+    },
+    {
+        ticker: "花蓮縣",
+        po:19,
+        revenuechange: -18,
+        mktcap: -9
+    },
+    {
+        ticker: "台中市",
+        po:9,
+        revenuechange: -5,
+        mktcap: -14
+    },
+    {
+        ticker: "台南市",
+        po:15,
+        revenuechange: -7,
+        mktcap: -14
+    },
+     {
+        ticker: "高雄市",
+        po:16,
+        revenuechange: -6,
+        mktcap: -16
+    },
+    {
+        ticker: "桃園市",
+        po:4,
+        revenuechange: -10,
+        mktcap: -16
+    },
+     {
+        ticker: "新北市",
+        po:3,
+        revenuechange: -16,
+        mktcap: -44
+    },
+    {
+        ticker: "台北市",
+        po:2,
+        revenuechange: -13,
+        mktcap: -63
+    }
+];
+var data4 = [
+    {
+        ticker: "台北市",
+        po:2,
+        revenuechange: -13,
+        mktcap: -63
+    },
+     {
+        ticker: "新北市",
+        po:3,
+        revenuechange: -16,
+        mktcap: -44
+    },
+    {
+        ticker: "桃園市",
+        po:4,
+        revenuechange: -10,
+        mktcap: -16
+    },
+    {
+        ticker: "高雄市",
+        po:16,
+        revenuechange: -6,
+        mktcap: -16
+    },
+    {
+        ticker: "台南市",
+        po:15,
+        revenuechange: -7,
+        mktcap: -14
+    },
+    {
+        ticker: "台中市",
+        po:9,
+        revenuechange: -5,
+        mktcap: -14
+    },
+    {
+        ticker: "花蓮縣",
+        po:19,
+        revenuechange: -18,
+        mktcap: -9
+    },
+    {
+        ticker: "彰化縣",
+        po:10,
+        revenuechange: -11,
+        mktcap: -9
+    },
+    {
+        ticker: "雲林縣",
+        po:12,
+        revenuechange: -17,
+        mktcap: -8
+    },
+    {
+        ticker: "嘉義縣",
+        po:14,
+        revenuechange: -22,
+        mktcap: -7
+    },
+    {
+        ticker: "新竹市",
+        po:5,
+        revenuechange: -9,
+        mktcap: -5
+    },
+    {
+        ticker: "苗栗縣",
+        po:8,
+        revenuechange: -8,
+        mktcap: -4
+    },
+    {
+        ticker: "南投縣",
+        po:11,
+        revenuechange: -8,
+        mktcap: -3
+    },
+    {
+        ticker: "澎湖縣",
+        po:20,
+        revenuechange: -10,
+        mktcap: -1
+    },
+    {
+        ticker: "金門縣",
+        po:21,
+        revenuechange: -7,
+        mktcap: -1
+    },
+    {
+       ticker: "台東縣",
+        po:18,
+        revenuechange: -6,
+        mktcap: -1
+    },
+    {
+        ticker: "嘉義市",
+        po:13,
+        revenuechange: -2,
+        mktcap: -1
+    },
+     {
+        ticker: "連江縣",
+        po:22,
+        revenuechange: 0,
+        mktcap: 0
+    },
+    {
+        ticker: "屏東縣",
+        po:17,
+        revenuechange: 2,
+        mktcap: 1
+    },
+    {
+        ticker: "宜蘭縣",
+        po:7,
+        revenuechange: 2,
+        mktcap: 1
+    },
+    {
+        ticker: "基隆市",
+        po:1,
+        revenuechange: 20,
+        mktcap: 3
+    },
+    {
+        ticker: "新竹縣",
+        po:6,
+        revenuechange: 26,
+        mktcap: 10
+    }
+];
+var aa = 1;
+var SA = 0;
+var SB = 0;
+
+createCharts(data1,data2,data3,data4,aa,SA,SB);
+
+function createCharts(raw,rbw,rcw,rdw,aa,Sa,Sb) {  
+    var chartElement = document.getElementById("charts"),
+        chartElementWidth = window.getComputedStyle(chartElement).width,
+        chartElementHeight = window.getComputedStyle(chartElement).height;
+    if(aa == 1){
+        var Configurations = {
+        // Set general configurations here...
+            data: raw,   
+            title: "近五年台灣各縣市實體書店變化",
+            html1: "#bar",
+            html2: "#tablet",
+            width: parseInt(chartElementWidth),
+            height: parseInt(chartElementHeight),
+            ratio: 0.8
+        };
+    }else if(aa == 2){
+        var Configurations = {
+        // Set general configurations here...
+            data: rbw,   
+            title: "近五年台灣各縣市實體書店變化",
+            html1: "#bar",
+            html2: "#tablet",
+            width: parseInt(chartElementWidth),
+            height: parseInt(chartElementHeight),
+            ratio: 0.8
+        };
+    }else if(aa == 3){
+        var Configurations = {
+        // Set general configurations here...
+            data: rcw,   
+            title: "近五年台灣各縣市實體書店變化",
+            html1: "#bar",
+            html2: "#tablet",
+            width: parseInt(chartElementWidth),
+            height: parseInt(chartElementHeight),
+            ratio: 0.8
+        };
+    }else if(aa == 4){
+        var Configurations = {
+        // Set general configurations here...
+            data: rdw,   
+            title: "近五年台灣各縣市實體書店變化",
+            html1: "#bar",
+            html2: "#tablet",
+            width: parseInt(chartElementWidth),
+            height: parseInt(chartElementHeight),
+            ratio: 0.8
+        };
+    }
+
+    var visualization = chart();
+
+    function chart() {
+        var data = Configurations.data, //	The Setup - takes configuration object properties...
+            title = Configurations.title,
+            html1 = Configurations.html1,
+            html2 = Configurations.html2,
+            width = Configurations.width,
+            height = Configurations.height;
+        ratio = Configurations.ratio;
+    
+        var returnGeneralData = function(d) {
+            return d;
+            }, //	return attributes
+            returnTicker = function(d) {
+                return d.ticker;
+            },
+            returnTickerUpcase = function(d) {
+                return d.ticker.toUpperCase();
+            },
+            returnRev = function(d) {
+                return d.revenuechange;
+            },
+            returnCap = function(d) {
+                return d.mktcap;    
+            };
+
+        var byPo = function(a, b){ 
+            return a.po > b.po ? 1 : b.po > a.po ? -1 : 0;
+            },
+            byPoInverse = function(a, b) {
+                return a.po < b.po ? 1 : b.po < a.po ? -1 : 0;
+            },
+            byCap = function(a, b) {
+                return a.mktcap > b.mktcap ? 1 : b.mktcap > a.mktcap ? -1 : 0;
+            },
+            byCapInverse = function(a, b) {
+                return a.mktcap < b.mktcap ? 1 : b.mktcap < a.mktcap ? -1 : 0;
+            };
+
+        var z = d3
+        .scaleLinear() //	both charts return the same color scale
+        .domain([d3.min(data.map(returnRev)), 0, d3.max(data.map(returnRev))])
+        .range(["red", "white","green"]);
+   
+        var highlightIDs = function(d) {
+            d3
+                .select("rect#" + d.ticker)
+                .attr("fill", d3.rgb(z(d.revenuechange)).darker(1));
+            d3
+                .select("tr#" + d.ticker )
+                .style("background-color", d3.rgb(z(d.revenuechange)).darker(1));
+        },
+            shadeIDs = function(d) {
+                d3.select("rect#" + d.ticker).attr("fill", z(d.revenuechange));
+                d3.select("tr#" + d.ticker).style("background-color", "white");
+            };
+
+        function genBarchart() {    
+      
+            var w = width * ratio;
+            var barchart = d3
+            .select(html1)
+            .append("svg")
+            .attr("width", w)
+            .attr("height", height);
+            var x = d3
+            .scaleBand()
+            .domain(data.map(returnTicker))
+            .rangeRound([10, w - 10])
+            .paddingInner(0.1),
+                xTicker = function(d) {
+                    return x(d.ticker);
+                };
+            var y = d3
+            .scaleLinear()
+            .domain([
+                d3.min(data.map(returnRev)) * (3 / 4),
+                d3.max(data.map(returnRev)) / (3 / 4)
+            ])
+            .nice()
+            .rangeRound([height - 10, 10]),
+                yRev = function(d) {      
+                    if (d.revenuechange > 0) { 
+                        return y(d.revenuechange);
+                    } else {
+                        return y(0);
+                    }
+                };
+       
+            var xAxis = d3.axisBottom().scale(x);
+            var yAxis = d3.axisLeft().scale(y);
+            var tooltip = d3.select("body").append("div").attr("class", "toolTip");
+    
+            barchart
+                .append("text")     
+                .attr("x", w / 2)
+                .attr("y", (height - 200) / 10)
+                .attr("text-anchor", "middle")
+                .style("font-size", "18px")
+                .text(title);
+            barchart // 長條圖設定
+                .selectAll("rect")
+                .data(data)
+                .enter()
+                .append("rect")
+                .attr("class", "bar")
+                .attr("id", returnTicker)
+                .attr("x", xTicker)
+                .attr("y", 252)
+                .on("mousemove", function(d) {
+                highlightIDs(d);
+                tooltip
+                    .style("left", d3.event.pageX - 50 + "px")
+                    .style("top", d3.event.pageY - 70 + "px")
+                    .style("display", "inline-block")
+                    .html(
+                    d.ticker.toUpperCase() +"<br>" +
+                    Math.round(d.revenuechange * 100) / 100 + "%"           
+                );
+            })
+                .on("mouseout", function(d) {
+                shadeIDs(d);
+                tooltip.style("display", "none");
+            })
+                .attr("width", x.bandwidth())
+                .attr("height", 0)
+            
+                .transition()
+                .duration(1500)
+                .attr("y",function(d){
+                if(d.revenuechange<0){return 252;}
+                else{return 252 - Math.abs(y(d.revenuechange) - y(0) );}
+            })
+                .attr("height", function(d) {
+                return Math.abs(y(d.revenuechange) - y(0) );
+            })
+                .attr("fill", function(d) {
+                return z(d.revenuechange);
+            })     
+        //}
+        //function genTable() {
+            var w = width * (1 - ratio);
+            var table = d3.select(html2).append("table");
+            var thead = table.append("thead"),
+                tbody = table.append("tbody");
+            var rows = tbody
+            .selectAll("tr")
+            .data(data)
+            .enter()
+            .append("tr")
+            .attr("id", returnTicker);
+            var headers = thead
+            .append("tr")
+            .selectAll("th")
+            .data(["縣市", "實際改變量(間)"])
+            .enter()
+            .append("th")
+            .text(returnGeneralData);
+            rows.append("td").text(returnTickerUpcase);
+            rows.append("td").text(returnCap);
+            rows
+                .on("mouseover", function(d) {
+                highlightIDs(d);
+            })
+                .on("mouseout", function(d) {
+                shadeIDs(d);
+            });
+            headers.on("click", sortOnClick);
+
+           function sortOnClick(d, i) {
+                if (i == 0) {
+                    if (Sa == 0) {
+                        barchart.remove();
+                        thead.remove();
+                        table.remove();
+                        headers.remove();
+                        rows.remove();
+                        aa = 2;                     
+                        createCharts(data1,data2,data3,data4,aa,1,0);
+                        rows.sort(byPoInverse);         
+                    } else {                       
+                        barchart.remove();
+                        thead.remove();
+                        table.remove();
+                        headers.remove();
+                        rows.remove();
+                        aa = 1;                     
+                        createCharts(data1,data2,data3,data4,aa,0,1);
+                        rows.sort(byPo);   
+                    }                 
+                }
+                if (i == 1) {
+                    if (Sb == 0) {
+                        barchart.remove();
+                        thead.remove();
+                        table.remove();
+                        headers.remove();
+                        rows.remove();
+                        aa = 3;                     
+                        createCharts(data1,data2,data3,data4,aa,0,1);
+                        rows.sort(byCap);
+                    } else {
+                        barchart.remove();
+                        thead.remove();
+                        table.remove();
+                        headers.remove();
+                        rows.remove();
+                        aa = 4;                     
+                        createCharts(data1,data2,data3,data4,aa,0,0);
+                        rows.sort(byCapInverse);
+                    }
+                    SwitchB++;
+                }
+                function oddOrEven(x) {
+                    return x & 1 ? "odd" : "even";
+                }
+            }
+        }
+        genBarchart();
+    }
+}
